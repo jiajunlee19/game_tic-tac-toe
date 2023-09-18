77444afd98f203d5ba361e34ce6ded543050f3e0
Jia Jun Lee <lee19@live.cn>
2023-09-18 19:22:08 +0800

    initial commit

---
+++ READ.md
@@ -0,0 +1,16 @@
+# Tic-Tac-Toe Game
+
+# Features
+1. Apply mark when a player clicks on any unmarked square in the 3x3 square matrix board
+2. For each round, alternate between 'X' and 'O' to represent two players' mark
+3. Indicates when a player has won the game 
+4. Indicates the game is tied when the board is full but there's no winner
+5. Stores a game’s history as a game progresses
+6. Allows players to timetravel to previous moves based on the game's history
+
+# Ideas
+1. For the current move only, show “You are at move #…” instead of a button.
+2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+3. Add a toggle button that lets you sort the moves in either ascending or descending order.
+4. When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
+5. Display the location for each move in the format (row, col) in the move history list.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function Square( {square, onSquareClick} ) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {square}
+    </button>
   );
+
 }
 
-export default App;
+function Board() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  function handleSquareClick(squareIndex) {
+    const currentBoard = board.slice();
+    currentBoard[squareIndex] = 'X';
+    setBoard(currentBoard);
+  };
+
+  // Assign boardHTML with row of 3 square, then loop 3x into 3x3 matrix
+  let boardHtml = [];
+  for (let j=0; j<9; j+=3) {
+
+    let rowHtml = [];  
+    for (let i=j; i<j+3; i++) {
+      rowHtml.push( <Square square={board[i]} onSquareClick={ () => handleSquareClick(i) } /> );
+    };
+
+    boardHtml.push( <div className='board-row'>{rowHtml}</div> );
+  };
+
+  return boardHtml;
+
+};
+
+export default Board;
