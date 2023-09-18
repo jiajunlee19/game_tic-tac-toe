a66a855c552ebf07811ad2f243bff5792626a217
Jia Jun Lee <lee19@live.cn>
2023-09-18 22:43:30 +0800

    completed basic game rules

--- src/App.js
+++ src/App.js
@@ -13,28 +13,99 @@ function Square( {square, onSquareClick} ) {
 
 function Board() {
 
+  //Define board state = array of 9: [null, null, ...null]
   const [board, setBoard] = useState(Array(9).fill(null));
 
+  //Game always start with player X, even-round is always Player X and odd-round is always Player O
+  const [isXNext, setIsXNext] = useState(false);
+
   function handleSquareClick(squareIndex) {
+
+    //break if square is already marked or there's a winner or board is fully marked
+    if (board[squareIndex] || calculateWinner(board) || checkIsBoardFull(board)) {
+      return;
+    }
+
     const currentBoard = board.slice();
-    currentBoard[squareIndex] = 'X';
+    
+    if (isXNext) {
+      currentBoard[squareIndex] = 'O';
+    }
+    else {
+      currentBoard[squareIndex] = 'X';
+    }
     setBoard(currentBoard);
+    setIsXNext(!isXNext);
+  };
+
+  //return True if board is fullly marked, game ties/no winner
+  function checkIsBoardFull (board) {
+    board.forEach((square) => {
+      if (square) {
+        return false;
+      }
+      return true;
+    });
+  };
+
+  //return Winner if winning condition met
+  function calculateWinner(board) {
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i=0; i<winningConditions.length; i++) {
+      const [a, b, c] = winningConditions[i]
+      if (board[a] && (board[a] === board[b]) && (board[a] === board[c])) {
+        return board[a];
+      }
+    };
+    return null;
+  };
+
+  // Show game status
+  function getGameStatus(board, isXNext) {
+    const winner = calculateWinner(board);
+    const isBoardFull = checkIsBoardFull(board);
+    let status = '';
+    if (winner) {
+      return 'Winner: ' + winner;
+    }
+    else if (!winner && isBoardFull) {
+      return 'Tie, no winner!';
+    }
+    else {
+      return 'Next Player: ' + (isXNext ? 'X': 'O');
+    }
   };
 
   // Assign boardHTML with row of 3 square, then loop 3x into 3x3 matrix
-  let boardHtml = [];
+  let boardHtml = [<div key='status' className='status'>{getGameStatus(board, isXNext)}</div>];
   for (let j=0; j<9; j+=3) {
 
     let rowHtml = [];  
     for (let i=j; i<j+3; i++) {
-      rowHtml.push( <Square square={board[i]} onSquareClick={ () => handleSquareClick(i) } /> );
+      rowHtml.push( <Square key={i} square={board[i]} onSquareClick={ () => handleSquareClick(i) } /> );
     };
 
-    boardHtml.push( <div className='board-row'>{rowHtml}</div> );
+    boardHtml.push( <div key={j} className='board-row'>{rowHtml}</div> );
   };
 
   return boardHtml;
 
 };
+
+// function Game() {
+
+//   // Define history = list of board arrays [ [null, ...null], ..., [null, ...null]  ]
+//   [history, setHistory] = useState([Array(9).fill(null)]);
+
+// };
 
 export default Board;
